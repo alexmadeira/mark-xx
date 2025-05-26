ed42cf310f8ec6d280fcfea8a120b2576dc44252
Alex Madeira <alex.c.madeira@gmail.com>
2025-05-25 23:03:53 -0300

    refactor: `color controller` optimization

--- src/@types/services/controller/color.ts
+++ src/@types/services/controller/color.ts
@@ -9,19 +9,11 @@ export const ZColors = z.object({
   variations: z.array(ZColor),
 })
 
-export const ZColorHex = z.string().regex(/^#([0-9A-Fa-f]{3}|[0-9A-Fa-f]{6})$/)
-export const ZColorRGB = z
-  .string()
-  .regex(/^\s*(25[0-5]|2[0-4]\d|1?\d{1,2})\s*,\s*(25[0-5]|2[0-4]\d|1?\d{1,2})\s*,\s*(25[0-5]|2[0-4]\d|1?\d{1,2})\s*$/)
-export const ZColorAlpha = z.number().min(0).max(1)
-
-export const ZColorData = z.tuple([ZColor, ZColorAlpha.default(1)])
-
 export const ZColorProps = z.object({
   default: ZEColorDefaultKey.default('dark'),
-  dark: z.union([ZColor, ZColorData]).default('#000000'),
-  light: z.union([ZColor, ZColorData]).default('#FFFFFF'),
-  variations: z.array(z.string()).default([]),
+  dark: ZColor.default('#000000'),
+  light: ZColor.default('#FFFFFF'),
+  variations: z.array(z.string()).optional(),
 })
 
 //
@@ -29,11 +21,7 @@ export const ZColorProps = z.object({
 //
 //
 
-export type TColorHex = z.infer<typeof ZColorHex>
-export type TColorRGB = z.infer<typeof ZColorRGB>
-export type TColorAlpha = z.infer<typeof ZColorAlpha>
 export type TColor = z.infer<typeof ZColor>
 export type TColors = z.infer<typeof ZColors>
-export type TColorData = z.infer<typeof ZColorData>
 
 export type TColorProps = z.infer<typeof ZColorProps>
