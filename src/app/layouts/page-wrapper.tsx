c745275b7384203673096650e9ecfc60e2eff8e6
Alex Madeira <alex.c.madeira@gmail.com>
2025-05-25 22:24:44 -0300

    feat: add logo color transition

--- src/app/layouts/page-wrapper.tsx
+++ src/app/layouts/page-wrapper.tsx
@@ -2,15 +2,17 @@ import { TPageWrapperProps } from '@/props/layouts/page-wrapper'
 
 import { useEffect } from 'react'
 
-import { scrollingController } from '_SRV/controller'
+import { overlapController, scrollingController } from '_SRV/controller'
 
 export function PageWrapper({ children }: TPageWrapperProps) {
   const ScrollingController = scrollingController()
+  const OverlapLogoController = overlapController('logo')
 
   ScrollingController.scrollTo(0, { immediate: true })
 
   useEffect(() => {
     ScrollingController.resize()
+    OverlapLogoController.reset()
   }, [])
 
   return children
