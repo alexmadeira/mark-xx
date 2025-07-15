6996ef9fe9861abeb427293e8992d29d3639d210
Alex Madeira <alex.c.madeira@gmail.com>
2025-07-15 12:04:11 -0300

    feat: add Project previews

--- src/app/pages/project/index.tsx
+++ src/app/pages/project/index.tsx
@@ -1,17 +1,21 @@
 import { Helmet } from 'react-helmet-async'
 
+import { twMerge } from 'tailwind-merge'
+
 import { Banner } from './sessions/banner'
 import { Header } from './sessions/header'
 import { Informations } from './sessions/informations'
+import { Previews } from './sessions/previews'
 
 export function Project() {
   return (
     <>
       <Helmet title="Project" />
       <Banner />
-      <div className="px-x-container relative top-0 left-0 z-2 w-full">
+      <div className={twMerge('relative top-0 left-0 z-2 w-full space-y-[clamp(2.5rem,3vw,8rem)]', 'mt-32')}>
         <Header />
         <Informations />
+        <Previews />
       </div>
     </>
   )
