43429cbd2700e83ef9cf874abaae5c39465818e1
Alex Madeira <alex.c.madeira@gmail.com>
2025-08-11 18:24:35 -0300

    style: project details

--- src/app/components/project/project-details/project-details-description.tsx
+++ src/app/components/project/project-details/project-details-description.tsx
@@ -2,7 +2,7 @@ import type { TProjectDetailsContentProps } from '@/props/pages/projects/project
 
 export function ProjectDetailsDescription(props: TProjectDetailsContentProps) {
   return (
-    <p className="group-data-[size='2x4']/masonry-item:5xl:block group-data-[size='2x2']/masonry-item:5xl:block group-data-[size='4x2']/masonry-item:4xl:block hidden w-full translate-y-5 text-[clamp(0.85rem,1vw,1.25rem)] leading-[clamp(1.45rem,1.5vw,2rem)] opacity-0 delay-150 duration-300 text-shadow-[color:var(--project-details-foreground-color)] text-shadow-md group-hover/masonry-item:translate-y-0 group-hover/masonry-item:opacity-100 group-data-[size='2x4']/masonry-item:hidden group-data-[size='4x2']/masonry-item:hidden group-data-[size='8x8']/masonry-item:block group-data-[size='4x4']/masonry-item:2xl:block">
+    <p className="group-data-[size='2x4']/masonry-item:5xl:block group-data-[size='2x2']/masonry-item:5xl:block group-data-[size='4x2']/masonry-item:4xl:block hidden w-full translate-y-5 text-[clamp(0.85rem,1vw,1.25rem)] leading-[clamp(1.45rem,1.5vw,2rem)] opacity-0 delay-150 duration-300 text-shadow-black text-shadow-md group-hover/masonry-item:translate-y-0 group-hover/masonry-item:opacity-100 group-data-[size='2x4']/masonry-item:hidden group-data-[size='4x2']/masonry-item:hidden group-data-[size='8x8']/masonry-item:block group-data-[size='4x4']/masonry-item:2xl:block">
       {props.description}
     </p>
   )
--- src/app/components/project/project-details/project-details-name.tsx
+++ src/app/components/project/project-details/project-details-name.tsx
@@ -2,7 +2,7 @@ import type { TProjectDetailsContentProps } from '@/props/pages/projects/project
 
 export function ProjectDetailsName(props: TProjectDetailsContentProps) {
   return (
-    <h2 className="group-data-[size='2x2']/masonry-item:4xl:block group-data-[size='2x4']/masonry-item:3xl:block 5xl:text-5xl translate-y-5 text-2xl font-semibold opacity-0 transition-all delay-75 duration-300 text-shadow-[color:var(--project-details-foreground-color)] text-shadow-md group-hover/masonry-item:translate-y-0 group-hover/masonry-item:opacity-100 sm:text-3xl group-data-[size='2x2']/masonry-item:sm:hidden group-data-[size='2x4']/masonry-item:sm:hidden md:text-4xl">
+    <h2 className="group-data-[size='2x2']/masonry-item:4xl:block group-data-[size='2x4']/masonry-item:3xl:block 5xl:text-5xl translate-y-5 text-2xl font-semibold opacity-0 transition-all delay-75 duration-300 text-shadow-black text-shadow-md group-hover/masonry-item:translate-y-0 group-hover/masonry-item:opacity-100 sm:text-3xl group-data-[size='2x2']/masonry-item:sm:hidden group-data-[size='2x4']/masonry-item:sm:hidden md:text-4xl">
       {props.name}
     </h2>
   )
