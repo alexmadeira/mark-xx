40095bb88c56bd55fdf34d83327373e69c21211f
Alex Madeira <alex.c.madeira@gmail.com>
2025-09-07 18:51:14 -0300

    style: header title and content

--- src/app/components/ui-element/page/header/header-title.tsx
+++ src/app/components/ui-element/page/header/header-title.tsx
@@ -15,11 +15,11 @@ export function HeaderTitle({ className, children, ...props }: TPageHeaderTitleP
   const page = useFetcherPages((st) => st.data[pageName])
 
   return (
-    <div {...props} className={twMerge('my-0 w-full', 'md:my-[clamp(1rem,4vw,5rem)]', className)}>
+    <div {...props} className={twMerge('my-0 w-full', 'sm:my-4', 'md:my-[clamp(1rem,4vw,5rem)]', className)}>
       <div className="px-x-container mx-auto flex w-full flex-col gap-0">
         <h1
           className={twMerge(
-            'text-black-900 w-full text-4xl tracking-widest sm:text-5xl',
+            'w-full text-4xl font-semibold tracking-widest sm:text-5xl',
             'md:text-[clamp(2rem,9vw,8rem)]',
             '4xl:text-[clamp(13rem,10vw,17rem)] leading-none',
           )}
