55d97e665333d42333ce93b4c3d92061f4ce3995
Alex Madeira <alex.c.madeira@gmail.com>
2025-09-07 18:24:29 -0300

    feat: responsive banner

--- src/app/components/header/index.tsx
+++ src/app/components/header/index.tsx
@@ -24,8 +24,8 @@ export function Header() {
     <div ref={headerRef} className="pointer-events-none fixed top-0 left-0 z-10 w-full">
       <div
         className={twMerge(
-          'h-full w-full pt-[clamp(calc(var(--spacing)_*_6),3vw,calc(var(--spacing)_*_10))]',
-          'pb-[clamp(calc(var(--spacing)_*_6),6vw,calc(var(--spacing)_*_20))]',
+          'h-full w-full pt-6 pb-8 sm:pb-6',
+          'md:pt-[clamp(calc(var(--spacing)_*_6),3vw,calc(var(--spacing)_*_10))] md:pb-[clamp(calc(var(--spacing)_*_6),6vw,calc(var(--spacing)_*_20))]',
           headerClassName,
         )}
       >
--- src/app/components/ui-element/page/header/header-title.tsx
+++ src/app/components/ui-element/page/header/header-title.tsx
@@ -15,11 +15,12 @@ export function HeaderTitle({ className, children, ...props }: TPageHeaderTitleP
   const page = useFetcherPages((st) => st.data[pageName])
 
   return (
-    <div {...props} className={twMerge('my-[clamp(1rem,4vw,5rem)] w-full', className)}>
+    <div {...props} className={twMerge('my-0 w-full', 'md:my-[clamp(1rem,4vw,5rem)]', className)}>
       <div className="px-x-container mx-auto flex w-full flex-col gap-0">
         <h1
           className={twMerge(
-            'text-black-900 w-full text-[clamp(2rem,9vw,8rem)] tracking-widest',
+            'text-black-900 w-full text-4xl tracking-widest sm:text-5xl',
+            'md:text-[clamp(2rem,9vw,8rem)]',
             '4xl:text-[clamp(13rem,10vw,17rem)] leading-none',
           )}
         >
