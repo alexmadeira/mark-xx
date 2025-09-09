e1f29da612edd43531b53428154e5ff943c512ca
Alex Madeira <alex.c.madeira@gmail.com>
2025-09-09 14:06:48 -0300

    feat: add motion transition color

--- src/@types/services/controller/color.ts
+++ src/@types/services/controller/color.ts
@@ -1,3 +1,5 @@
+import type { MotionValue, ValueTransition } from 'motion'
+
 import { z } from 'zod/v4'
 
 export const ZColor = z.string()
@@ -6,6 +8,8 @@ export const ZColors = z.object({
   light: ZColor,
   variations: z.array(ZColor),
 })
+export const ZColorMotionColor = z.custom<MotionValue<z.infer<typeof ZColor>>>()
+export const ZColorMotionColors = z.map(z.string(), ZColorMotionColor)
 export const ZColorCssVars = z.record(z.string(), z.string())
 
 export const ZColorsBetterContrastResult = z
@@ -19,6 +23,7 @@ export const ZColorProps = z.object({
   dark: ZColor.default('#000000'),
   light: ZColor.default('#FFFFFF'),
   variations: z.array(z.string()).optional(),
+  transition: z.custom<ValueTransition>().optional(),
 })
 
 //
@@ -28,6 +33,10 @@ export const ZColorProps = z.object({
 
 export type TColor = z.infer<typeof ZColor>
 export type TColors = z.infer<typeof ZColors>
+
+export type TColorMotionColor = z.infer<typeof ZColorMotionColor>
+export type TColorMotionColors = z.infer<typeof ZColorMotionColors>
+
 export type TColorCssVars = z.infer<typeof ZColorCssVars>
 
 export type TColorsBetterContrastResult = z.infer<typeof ZColorsBetterContrastResult>
