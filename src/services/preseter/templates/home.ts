11ff0306f2a49ad82dd29edecb1525a1c7c2253c
Alex Madeira <alex.c.madeira@gmail.com>
2025-09-09 16:33:43 -0300

    feat: add storage version to invalidade data

--- src/services/preseter/templates/home.ts
+++ src/services/preseter/templates/home.ts
@@ -2,6 +2,6 @@ import type { TContentPreseterTemplates } from '@/services/preseter/content'
 
 export const homeTemplates = {
   paragraph: `
-  <p class="text-[clamp(0.875rem,2vw,2.25rem)] leading-[clamp(1.25rem,2vw,2.5rem)] font-light transition-colors duration-200">{{text}}</p>
+  <p class="text-[clamp(0.875rem,2vw,2.25rem)] leading-[clamp(1.25rem,2vw,2.5rem)] font-light">{{text}}</p>
   `.trim(),
 } satisfies TContentPreseterTemplates
