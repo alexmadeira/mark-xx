18551f42b64f6b945d1dd102c39ae5cac6b42645
Alex Madeira <alex.c.madeira@gmail.com>
2025-09-09 21:37:16 -0300

    fix: use indexDB in production

--- src/app/components/project/project-details/project-details-image.tsx
+++ src/app/components/project/project-details/project-details-image.tsx
@@ -7,11 +7,10 @@ export function ProjectDetailsImage(props: TProjectDetailsContentProps) {
   return (
     <div className="h-full w-full overflow-clip">
       <img
-        // src={props.bannerSrc}
-        src={`https://picsum.photos/1000/1000?randon=${_.uniqueId()}&grayscale`}
+        src={props.bannerSrc}
         alt={props.bannerName}
         className={twMerge(
-          'h-full min-h-full w-full min-w-full scale-[1.005] object-contain transition-all duration-[2s] group-hover/masonry-item:scale-120 group-hover/masonry-item:duration-[8s]',
+          'h-full min-h-full w-full min-w-full scale-[1.005] object-contain grayscale transition-all duration-[2s] group-hover/masonry-item:scale-120 group-hover/masonry-item:duration-[8s]',
           props.bannerClass,
         )}
       />
