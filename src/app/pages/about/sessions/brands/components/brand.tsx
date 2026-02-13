6b289219661e23c2fb373ab36311b096e8fdd555
Alex Madeira <alex.c.madeira@gmail.com>
2026-02-13 14:05:24 -0300

    feat: update sessions space and fix responsive show brands

--- src/app/pages/about/sessions/brands/components/brand.tsx
+++ src/app/pages/about/sessions/brands/components/brand.tsx
@@ -2,12 +2,18 @@ import type { TBrandsBrandProps } from '@/props/pages/about/brands'
 
 import { twMerge } from 'tailwind-merge'
 
-export function Brand({ logo, name, className, ...rest }: TBrandsBrandProps) {
+export function Brand({ logo, name, className, index, ...rest }: TBrandsBrandProps & { index: number }) {
   return (
     <div
       {...rest}
+      data-xs={index < 3}
+      data-sm={index <= 3}
+      data-md={index <= 5}
+      data-lg={index <= 7}
+      data-xl={index <= 12}
       className={twMerge(
-        'group relative z-0 flex w-40 p-[clamp(0.5rem,1vw,1rem)] grayscale transition-all duration-250 hover:z-1 hover:scale-110 hover:grayscale-0',
+        'relative z-0 flex-1 p-[clamp(0.5rem,1vw,1rem)] grayscale transition-all duration-250 hover:z-1 hover:scale-110 hover:grayscale-0',
+        'hidden data-[xs=true]:flex sm:data-[sm=true]:flex md:data-[md=true]:flex lg:data-[lg=true]:flex xl:data-[xl=true]:flex 2xl:flex',
         className,
       )}
     >
