19427b16e3eb20ec7f5088c981d97bfe32ee1e75
Alex Madeira <alex.c.madeira@gmail.com>
2026-02-17 18:36:47 -0300

    fix: remove background

--- src/app/pages/project/sessions/informations/description.tsx
+++ src/app/pages/project/sessions/informations/description.tsx
@@ -20,6 +20,8 @@ export function Description() {
         'flex w-full flex-col gap-[clamp(0.5rem,1vw,1.5rem)]',
         '[&_p]:text-[clamp(0.875rem,1vw,1.5rem)] [&_p]:leading-[calc(1.25/0.875),2vw,calc(2/1.5)] [&_p]:font-light',
         '[&_h2]:mt-[clamp(0.5rem,1.5vw,2rem)] [&_h2]:text-[clamp(1.25rem,1.85vw,3.25rem)] [&_h2]:leading-[calc(1.75/1.25),2vw,2.25] [&_h2]:font-medium [&_h2]:first:mt-0',
+        '[&_ol]:list-inside [&_ol]:list-decimal [&_ol]:text-[clamp(0.875rem,1vw,1.5rem)] [&_ol]:leading-[calc(1.25/0.875),2vw,calc(2/1.5)] [&_ol]:font-light [&_ol]:marker:font-medium',
+        '[&_ul]:list-inside [&_ul]:list-disc [&_ul]:text-[clamp(0.875rem,1vw,1.5rem)] [&_ul]:leading-[calc(1.25/0.875),2vw,calc(2/1.5)] [&_ul]:font-light [&_ul]:marker:font-medium',
       )}
       dangerouslySetInnerHTML={{ __html: project.content }}
     />
