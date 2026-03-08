fbf39f71298e5af45dcd39eff84bad9633183c3f
Alex Madeira <alex.c.madeira@gmail.com>
2026-03-08 14:47:09 -0300

    feat: add score ratio

--- src/game/snake/game.ts
+++ src/game/snake/game.ts
@@ -1,4 +1,5 @@
 import type { GameController } from './game-controller'
+import type { ScoreSystem } from './systems/score-system'
 import type { SnakeKeyboardInput } from '_GAME/snake/application/input/snake-keyboard-input'
 import type { SnakeFood } from '_GAME/snake/entity/snake-food'
 import type { SnakePlayer } from '_GAME/snake/entity/snake-player'
@@ -7,12 +8,15 @@ import type { SnakeCollision } from '_GAME/snake/services/snake-collision'
 import { GameLogic } from '_GAME/core/application/game-logic'
 
 export class Game extends GameLogic {
-  private speed = 150
+  private speed = 0
+  private speedRatio = 4
+  private baseSpeed = 120
   private minInterval = 50
 
   private controller!: GameController
 
   constructor(
+    private readonly scoreSystem: ScoreSystem,
     private readonly collision: SnakeCollision,
     private readonly keyboardInput: SnakeKeyboardInput,
     public readonly food: SnakeFood,
@@ -21,27 +25,22 @@ export class Game extends GameLogic {
     super()
   }
 
-  private increaseSpeed() {
-    this.speed = Math.max(this.minInterval, this.speed - 5)
-    // const score = this.snake.length
+  private updateSpeed() {
+    const factor = Math.sqrt(this.scoreSystem.value)
 
-    //  this.tickInterval = Math.max(
-    //    this.minInterval,
-    //    120 - score * 2
-    //  )
+    this.speed = Math.max(this.minInterval, this.baseSpeed - factor * this.speedRatio)
   }
 
   private collisionHandler() {
     if (this.collision.checkSelfCollision()) this.gameOver()
     if (this.collision.checkWallCollision()) this.gameOver()
-    // if (this.collision.checkWallCollision()) this.snake.setPosition(this.collision.contrarySide)
   }
 
   private updateFood() {
     if (this.snake.position.equals(this.food.position) || this.snake.nextPosition.equals(this.food.position)) {
-      this.food.consume()
       this.snake.grow()
-      this.increaseSpeed()
+      this.food.consume()
+      this.updateSpeed()
     }
 
     this.food.respawn()
@@ -67,12 +66,14 @@ export class Game extends GameLogic {
   }
 
   public restart() {
-    this.speed = 150
+    this.speed = this.baseSpeed
     this.food.init()
     this.snake.init()
+    this.scoreSystem.reset()
   }
 
   public update() {
+    console.log(this.speed)
     this.actionsHandler()
     this.collisionHandler()
 
@@ -82,5 +83,9 @@ export class Game extends GameLogic {
 
   public get tickInterval() {
     return this.speed
+  }
+
+  public get score() {
+    return this.scoreSystem.value
   }
 }
