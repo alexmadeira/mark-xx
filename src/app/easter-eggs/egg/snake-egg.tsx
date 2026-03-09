b60b4da91ba47ba28543b80f33d28518657b9ee8
Alex Madeira <alex.c.madeira@gmail.com>
2026-03-09 16:41:12 -0300

    fix: add lazy loader to import game

--- src/app/easter-eggs/egg/snake-egg.tsx
+++ src/app/easter-eggs/egg/snake-egg.tsx
@@ -1,20 +1,26 @@
-import { SnakeGame } from '_APP/games/snake'
+import { lazy, Suspense } from 'react'
+
 import { Portal } from '@radix-ui/react-portal'
 
 import { useEasterEgg } from '_STR/useEasterEgg'
+
+const SnakeGame = lazy(() => import('_APP/games/snake').then((m) => ({ default: m.SnakeGame })))
 
 export function SnakeEgg() {
   const snakeEgg = useEasterEgg((state) => state.data.eggs.snake)
   const showSnakeEgg = snakeEgg?.status === 'called'
 
   if (!showSnakeEgg) return null
+
   return (
     <Portal className="pointer-events-none select-none">
       <div
         key="snake-egg"
         className="fixed top-1/2 left-1/2 z-20 flex aspect-square min-h-[70vmin] min-w-[70vmin] -translate-x-1/2 -translate-y-1/2 items-center justify-center overflow-clip"
       >
-        <SnakeGame />
+        <Suspense fallback={null}>
+          <SnakeGame />
+        </Suspense>
       </div>
     </Portal>
   )
