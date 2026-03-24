9b494a3ac6c311279c4fea363bf9317ae16c3efd
Alex Madeira <alex.c.madeira@gmail.com>
2026-03-24 20:27:36 -0300

    fix: project hover

--- src/app/components/project/project-details/project-details-image.tsx
+++ src/app/components/project/project-details/project-details-image.tsx
@@ -1,12 +1,16 @@
 import type { TProjectDetailsContentProps } from '@/props/pages/projects/project-details'
 
 import { Image } from '_APP/components/ui-element/image'
-import _ from 'lodash'
 import { twMerge } from 'tailwind-merge'
 
 export function ProjectDetailsImage(props: TProjectDetailsContentProps) {
   return (
-    <div className={twMerge('_overflow-clip relative h-full w-full', props.thumbnailClass)}>
+    <div
+      className={twMerge(
+        'h-full min-h-full w-full min-w-full object-cover transition-all duration-[20s] group-hover/masonry-item:scale-105 group-hover/masonry-item:duration-[8s]',
+        props.thumbnailClass,
+      )}
+    >
       <Image src={props.thumbnail} alt={props.bannerName} className="object-cover" />
     </div>
   )
