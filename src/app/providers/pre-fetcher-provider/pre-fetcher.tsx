447fb7794514652a2fdb8158083d8e2120cf6fc7
Alex Madeira <alex.c.madeira@gmail.com>
2026-04-02 15:44:32 -0300

    fix: preload

--- src/app/providers/pre-fetcher-provider/pre-fetcher.tsx
+++ src/app/providers/pre-fetcher-provider/pre-fetcher.tsx
@@ -1,39 +1,20 @@
-import {
-  awardsFetcher,
-  brandsFetcher,
-  networksFetcher,
-  pageFetcher,
-  preFetcher,
-  projectsFetcher,
-  repositoriesFetcher,
-  technologiesFetcher,
-} from '_SRV/fetcher'
-
-import { env } from '~/env'
+import { networksFetcher, pageFetcher, preFetcher, projectsFetcher, technologiesFetcher } from '_SRV/fetcher'
 
 export function PreFetcher() {
   const FPage = pageFetcher()
-  const FAwards = awardsFetcher()
-  const FBrands = brandsFetcher()
+
   const FProjects = projectsFetcher()
   const FNetworks = networksFetcher()
   const FTechnologies = technologiesFetcher()
-  const FRepositories = repositoriesFetcher()
 
   const FPreFetcher = preFetcher()
 
   FPreFetcher.addPrefetcher([
     FPage.prefetch('home'),
-    FPage.prefetch('about'),
-    FPage.prefetch('projects'),
-    FAwards.prefetch('about:awards'),
-    FBrands.prefetch('about:brands'),
     FNetworks.prefetch('networks'),
-    FProjects.prefetch('all:projects'),
     FProjects.prefetch('home:projects', { filter: { fields: { highlight: true } } }),
     FTechnologies.prefetch('banner:technologies', { filter: { tags: ['banner'] } }),
     //
-    FRepositories.prefetch('about:repositories', { params: { perPage: env.VITE_GITHUB_TOTAL_REPOSITORIES } }),
   ])
 
   return null
